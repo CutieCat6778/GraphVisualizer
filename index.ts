4284974e806dbc8331ad2a3d5fcf2ecf530fa5f2
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2024-01-24 14:10:51 +0100

    [UPDATE] fixed the identifier

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 let func = "Math.pow(x, 2)";
-const x = 10;
+const x = 6;
 const min = x * -1;
 const max = x;
 let matrix: string[][] = [];
 const highestPoint = calculatePoint(max).val;
 const schritt = 1;
-const viewDistanceInX = 4;
+const viewDistanceInX = 14;
+const identifier = "##";
 
 function initalizeMatrix() {
     for (let i = 0; i <= highestPoint; i += schritt) {
@@ -24,16 +25,16 @@ function main() {
 
     for (let i = 0; i <= max + (Math.abs(min)); i += schritt) {
         const res = calculatePoint(i - (Math.abs(min)));
-        matrix[res.val][i] = "#";
+        matrix[res.val][i] = identifier;
     }
 
     for (let i = highestPoint; i > 0; i -= schritt) {
         console.log((matrix[i].join(" ")) + "\n");
     }
 
-	const placement = matrix[0].indexOf("#");
+	const placement = matrix[0].indexOf(identifier);
 	matrix[0][placement] = " ".repeat(viewDistanceInX);
-	matrix[0][placement-1] = "#";
+	matrix[0][placement-1] = identifier;
 	matrix[0][placement-2] = " ".repeat(viewDistanceInX + Math.round(viewDistanceInX / 2) + 1);
 	console.log(matrix[0].join(" "));
 }
